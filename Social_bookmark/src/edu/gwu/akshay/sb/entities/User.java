1e5533fee5895726899500c3295e0f899c4041ea
agsrc <akshay0712@gwu.edu>
2018-06-27 16:55:13 -0400

    first commit

---
+++ Social_bookmark/src/edu/gwu/akshay/sb/entities/User.java
@@ -0,0 +1,62 @@
+package edu.gwu.akshay.sb.entities;
+
+public class User {
+	private long id;
+	private String email;
+	private String password;
+	private String firstname;
+	private String lastname;
+	private String userType;
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+
+}
